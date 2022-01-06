c03a25913233349953cf805aeea011f527c5a9c3
echoqshen <90880564+echoqshen@users.noreply.github.com>
2022-01-05 20:36:21 -0500

    Create guess_game_while_loop.py

---
+++ guess_game_while_loop.py
@@ -0,0 +1,11 @@
+my_number = 6
+guess_count = 0
+guess_limit = 3
+while guess_count <guess_limit:
+    guess = int(input('Guess: '))
+    guess_count+=1
+    if guess==my_number:
+        print('You Won!')
+        break
+else:
+    print("You lost!")
